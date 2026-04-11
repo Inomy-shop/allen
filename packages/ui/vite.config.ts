840aa6326c282fa1878f9eb6d73448dd0c12109d
Shreemant Kumar <shreemantkumar@Shreemants-MacBook-Pro.local>
2026-04-11 17:54:44 +0530

    proxy fix

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: uiPort,
+      hmr: uiPort !== 5173 ? false : undefined,
       proxy: {
         '/api': {
           target: `http://127.0.0.1:${apiPort}`,
